f8465d1869bccb9bfc2f0b2810ee136891e05018
lela <130366723+lelabutture@users.noreply.github.com>
2023-09-11 08:51:22 -0300

    esse atualizado

---
+++ JavaScript-main/introducao-javascript/js/principal.js
@@ -0,0 +1,13 @@
+//console.log("Jesus salva e liberta");
+
+//------------------Daqui para baixo começa o codigo---------------------
+
+//alteração do titulo
+//console.log(document);
+//cria a variável titulo e armazena o conteudo da classe
+let titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//atribuição de um novo valor na variável titulo 
+titulo.textContent = "Fake Natty Nutrição!";
+
+//alteração na tabela
